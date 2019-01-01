77125167337c71625f2289d434f934658dee2917
pgonzales <pgonzales@ozon.ru>
2019-01-01 18:55:25 +0300

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,44 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parser: "babel-eslint",
   env: {
     browser: true,
     node: true
   },
-  extends: 'standard',
+  extends: "eslint:recommended",
   // required to lint *.vue files
   plugins: [
-    'html'
+    "html"
   ],
   // add your custom rules here
-  rules: {},
+  rules: {
+    "space-before-function-paren": [
+        "error",
+        "never"
+    ],
+    "object-curly-spacing": [
+      "error", 
+      "never"
+    ],
+    "no-mixed-operators": 0,
+    "no-undef": 1,
+        "no-console": 1,
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+  },
   globals: {}
 }
